e63dde10c60a26e7f1e1f921970dc7271e4547a1
EMANUEL MUÑOZ <emanuel.guillo@gmail.com>
2025-01-31 16:17:32 -0500

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, BigInteger, ARRAY, String
-from database import Base
-
-class RegistroDeIngreso(Base):
-    __tablename__ = "registrosdeingreso"
-
-    idregistro = Column(BigInteger, primary_key=True, index=True)
-    documentoingre = Column(ARRAY(String))  # Cambiar CHAR a String
-    nombrepersona = Column(ARRAY(String))  # Cambiar CHAR a String
